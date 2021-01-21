dc933170783700fef00266cbb33ae2a2bcb0b49a
Igor Mukhametdinov <58729766+mukhametdinovigor@users.noreply.github.com>
2021-01-21 22:24:50 +1100

    Simplify try-except block

--- main.py
+++ main.py
@@ -34,17 +34,15 @@ def main():
     user_url = args.link
     link_without_https = f'{urlparse(user_url).netloc}{urlparse(user_url).path}'
 
-    if requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{link_without_https}/clicks/summary',
-                    headers=headers).ok:
-        clicks_count = count_clicks(bitly_token, link_without_https)
-        print(f'Всего переходов по ссылке - {clicks_count}')
-    else:
-        try:
-            bitlink = shorten_link(bitly_token, user_url)
+    try:
+        if requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{link_without_https}/clicks/summary', headers=headers).ok:
+            clicks_count = count_clicks(BITLY_TOKEN, link_without_https)
+            print(f'Всего переходов по ссылке - {clicks_count}')
+        else:
+            bitlink = shorten_link(BITLY_TOKEN, user_url)
             print(f'Битлинк - {bitlink}')
-        except requests.exceptions.HTTPError:
-            print('Произошла ошибка. Вы ввели неверную ссылку.')
-
+    except requests.exceptions.HTTPError:
+        print('Произошла ошибка. Вы ввели неверную ссылку.')
 
 if __name__ == '__main__':
     main()
